49a027585c2bf8f9aa69d50f61a2827cd3a48f96
Robbert Wijtman <robbert@xinxmedia.com>
2022-01-27 21:32:28 -0800

    Update packages

--- examples/webpack.config.base.js
+++ examples/webpack.config.base.js
@@ -15,48 +15,52 @@ module.exports = (dirname) => {
 				'@bucky24/react-canvas-terrain': path.resolve(__dirname, '../src/index.js'),
 			}
 		},
+		mode: "development",
 		module: {
 			rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                [require.resolve('@babel/preset-env'), {
+                                    exclude: ["transform-regenerator"],
+                                }],
+                                '@babel/preset-react'
+                            ],
+                            plugins: ["@babel/plugin-proposal-class-properties"]
+                        },
+                    },
+                },
+                {
+                    test: /\.css$/,
+                    loader: 'style-loader'
+                },
+                {
+                    test: /\.css$/,
+                    loader: 'css-loader',
+                    options: {
+                        modules: {
+                            localIdentName: "[path][name]__[local]--[hash:base64:5]",
+                        },
+                    },
+                }, 
 				{
-					test: /\.js$/,
-					exclude: /node_modules/,
-					use: {
-						loader: 'babel-loader',
-						options: {
-							presets: [
-								require.resolve('@babel/preset-env'),
-								'@babel/preset-react'
-							],
-							plugins: ["@babel/plugin-proposal-class-properties"]
-						}
-					}
-				},
-				{
-					test: /\.css$/,
-					loader: 'style-loader'
-				},
-				{
-					test: /\.css$/,
-					loader: 'css-loader',
-					query: {
-						modules: true,
-						localIdentName: '[name]__[local]___[hash:base64:5]'
-					}
-				},
-				{
-					test: /\.(jpe?g|png|gif|svg)$/i,
-					loaders: [
-						'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
-						{
-							loader: 'image-webpack-loader',
-							query: {
-								optipng: {
-									optimizationLevel: 4,
-								}
-							}
-						}
-					]
-				}
+                    test: /\.(jpe?g|png|gif|svg)$/i,
+                    use: {
+                        loader: 'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
+                        options: {
+                            loader: 'image-webpack-loader',
+                            options: {
+                                optipng: {
+                                    optimizationLevel: 4,
+                                }
+                            }
+                        }
+                    }
+                }
 			]
 		},
 		stats: {
